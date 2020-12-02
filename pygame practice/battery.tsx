747b07e6e236cbda459fca72dd88bda440958a46
styssk <75117306+styssk@users.noreply.github.com>
2020-12-02 18:37:26 +0000

    Delete battery.tsx

--- pygame practice/battery.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="battery" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="battery.png" trans="ff00ff" width="64" height="64"/>
-</tileset>
