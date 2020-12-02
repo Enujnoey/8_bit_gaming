891423792d48bae11cb81ab2850bfd4b88581a66
styssk <75117306+styssk@users.noreply.github.com>
2020-12-02 15:57:34 +0000

    images
    the images used for the game (some of them might have not be used but i don't remember anymore)

---
+++ images/test3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test3" tilewidth="64" tileheight="64" tilecount="240" columns="20">
+ <image source="../../../Downloads/Webp.net-resizeimage.jpg" width="1280" height="800"/>
+</tileset>
