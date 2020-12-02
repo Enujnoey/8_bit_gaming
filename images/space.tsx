d8f667682f87b5ed3cea60767667e5fe1b2635ed
styssk <75117306+styssk@users.noreply.github.com>
2020-12-02 18:08:24 +0000

    Delete space.tsx

--- images/space.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="space" tilewidth="64" tileheight="64" tilecount="126" columns="14">
- <image source="../../../Downloads/Grid2.png" width="900" height="600"/>
-</tileset>
