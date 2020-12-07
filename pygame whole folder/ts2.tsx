191f2db898b5e1752587a061a42d89fe2726cb76
styssk <75117306+styssk@users.noreply.github.com>
2020-12-07 19:43:11 +0200

    Delete ts2.tsx

--- pygame whole folder/ts2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="ts2" tilewidth="64" tileheight="64" spacing="30" tilecount="64" columns="8">
- <image source="SciFi_Warehouse_C2.png" trans="ff00ff" width="768" height="768"/>
-</tileset>
