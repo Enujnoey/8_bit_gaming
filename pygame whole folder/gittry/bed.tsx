7e72569436bd05917fec3670553996b3139188b6
styssk <75117306+styssk@users.noreply.github.com>
2020-12-07 19:33:23 +0200

    Delete bed.tsx

--- pygame whole folder/gittry/bed.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="bed" tilewidth="64" tileheight="64" tilecount="8" columns="4">
- <image source="bed.png" trans="ff00ff" width="270" height="128"/>
-</tileset>
