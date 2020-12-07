50e5c4296ce7fc53df77b0e4d605f4c71f338e8f
styssk <75117306+styssk@users.noreply.github.com>
2020-12-07 19:34:59 +0200

    Delete test6.tsx

--- pygame whole folder/gittry/test6.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="test6" tilewidth="16" tileheight="16" tilecount="198" columns="18">
- <image source="../../preview-top-down-sci-fi.png" width="288" height="176"/>
-</tileset>
