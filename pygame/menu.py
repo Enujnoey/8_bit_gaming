2d8888a8493780fb47f9ce8c58ae6425cbd731ab
enujnoey mik <35134971+Enujnoey@users.noreply.github.com>
2020-12-09 21:54:42 +0000

    Update menu.py

--- pygame/menu.py
+++ pygame/menu.py
@@ -1,5 +1,12 @@
-import pygame as pg
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 17:52:43 2020
 
+@author: enujnoey
+"""
+
+import pygame as pg
 
 class Menu():
     def __init__(self, game):
@@ -10,13 +17,12 @@ class Menu():
         self.offset = - 100
 
     def draw_cursor(self):
-        self.game.draw_text("*", 35, self.cursor_rect.x, self.cursor_rect.y)
-            
+        self.game.draw_text('*', 35, self.cursor_rect.x, self.cursor_rect.y)
+
     def blit_screen(self):
         self.game.window.blit(self.game.screen, (0, 0))
         pg.display.update()
         self.game.reset_keys()
-
 
 class MainMenu(Menu):
     def __init__(self, game):
@@ -28,68 +34,73 @@ class MainMenu(Menu):
         self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
 
     def display_menu(self):
-        self.run_dispplay = True
         self.game.font_name = self.game.font_title
+        self.run_display = True
         while self.run_display:
             self.game.check_events()
             self.check_input()
             self.game.screen.fill(self.game.BLACK)
-            self.game.draw_text("Stranded", 40, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 -40)
+            self.game.draw_text('Stranded', 80, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 60)
             self.game.draw_text("Play Game", 40, self.startx, self.starty)
             self.game.draw_text("Options", 40, self.optionsx, self.optionsy)
             self.game.draw_text("Credits", 40, self.creditsx, self.creditsy)
             self.draw_cursor()
             self.blit_screen()
 
+
     def move_cursor(self):
         if self.game.DOWN_KEY:
-            if self.state == "Start":
+            if self.state == 'Start':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
-                self.state = "Options"
-            elif self.state == "Options":
+                self.state = 'Options'
+                
+            elif self.state == 'Options':
                 self.cursor_rect.midtop = (self.creditsx + self.offset, self.creditsy)
-                self.state = "Credits"
-            elif self.state == "Credits":
+                self.state = 'Credits'
+                
+            elif self.state == 'Credits':
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
-                self.state = "Start"
-        if self.game.UP_KEY:
-            if self.state == "Start":
+                self.state = 'Start'
+                
+        elif self.game.UP_KEY:
+            if self.state == 'Start':
                 self.cursor_rect.midtop = (self.creditsx + self.offset, self.creditsy)
-                self.state = "Credits"
-            elif self.state == "Options":
+                self.state = 'Credits'
+                
+            elif self.state == 'Options':
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
-                self.state = "Start"
-            elif self.state == "Credits":
+                self.state = 'Start'
+                
+            elif self.state == 'Credits':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
-                self.state = "Options"
-
+                self.state = 'Options'
 
     def check_input(self):
         self.move_cursor()
         if self.game.START_KEY:
-            if self.state == "Start":
+            if self.state == 'Start':
                 self.game.playing = True
-            elif self.state == "Options":
+            elif self.state == 'Options':
                 self.game.curr_menu = self.game.options
-            elif self.state == "Credits":
+            elif self.state == 'Credits':
                 self.game.curr_menu = self.game.credits
+            self.run_display = False
 
 class OptionsMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
-        self.state = "Volume"
+        self.state = 'Volume'
         self.volx, self.voly = self.mid_w, self.mid_h + 40
         self.controlsx, self.controlsy = self.mid_w, self.mid_h + 80
         self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
 
     def display_menu(self):
-        self.run_dispplay = True
-        self.game.font_name = self.game.font_title #need to clarify the font
+        self.run_display = True
         while self.run_display:
             self.game.check_events()
             self.check_input()
             self.game.screen.fill(self.game.BLACK)
-            self.game.draw_text("Options", 40, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 -40)
+            self.game.draw_text('Options', 40, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 40)
             self.game.draw_text("Volume", 35, self.volx, self.voly)
             self.game.draw_text("Controls", 35, self.controlsx, self.controlsy)
             self.draw_cursor()
@@ -99,19 +110,16 @@ class OptionsMenu(Menu):
         if self.game.BACK_KEY:
             self.game.curr_menu = self.game.main_menu
             self.run_display = False
-
         elif self.game.UP_KEY or self.game.DOWN_KEY:
-            if self.state == "Volume":
-                self.state = "Controls"
-                self.cursor_rect.midtop: (self.controlsx + self.offset, self.coltrolsy)
-
-            elif self.state == "Controls":
-                self.state = "Volume"
+            if self.state == 'Volume':
+                self.state = 'Controls'
+                self.cursor_rect.midtop = (self.controlsx + self.offset, self.controlsy)
+            elif self.state == 'Controls':
+                self.state = 'Volume'
                 self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
         elif self.game.START_KEY:
-            #create a volume / controls menu and implement a control
+            # TO-DO: Create a Volume Menu and a Controls Menu
             pass
-
 
 class CreditsMenu(Menu):
     def __init__(self, game):
@@ -128,3 +136,5 @@ class CreditsMenu(Menu):
             self.game.draw_text("Credits", 40, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 40)
             self.game.draw_text("8 Bit Gaming", 35, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 10)
             self.blit_screen()
+
+
